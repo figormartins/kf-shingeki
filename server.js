e3a5eee7e142ddc63ae2de6519e751e7e125e1d1
Igor Martins <figortmartins@gmail.com>
2022-09-13 13:01:32 -0300

    feat: getting and waiting for exact time to attack

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const { kfAttackTimeToFullDate, millisToMinutesAndSeconds } = require('./helpers
         await page.click("#id_terms_accepted");
         await page.click("#content > div > div:nth-child(1) > form > table > tbody > tr:nth-child(7) > td:nth-child(2) > input");
         await page.waitForNavigation();
-        console.log("Cadastrou...");
+        console.log("Cadastrou...", email);
         
         // Select world
         await page.goto('https://moonid.net/games/knightfight/');
@@ -46,44 +46,49 @@ const { kfAttackTimeToFullDate, millisToMinutesAndSeconds } = require('./helpers
         await page.waitForSelector("#page > div.box-charimage > img.charimage");
         console.log("Registrou...");
 
-        // Wait to search
-        if (timeToAttack) {
-            await page.waitForSelector("#kftime-header");
-            const valueTimeNow = await page.$eval("#kftime-header", el => el.textContent);
-            const [hours, minutes, seconds] = valueTimeNow.split(":");
-            const timeNow = new Date(timeToAttack.valueOf());
+        try {
+            // Wait to search
+            if (timeToAttack) {
+                await page.reload();
+                await page.waitForSelector("#kftime-header");
+                const valueTimeNow = await page.$eval("#kftime-header", el => el.textContent);
+                const [hours, minutes, seconds] = valueTimeNow.split(":");
+                const timeNow = new Date(timeToAttack.valueOf());
 
-            if (hours < timeNow.getHours()) {
-                timeNow.setDate(timeNow.getDate() + 1);
+                if (hours < timeNow.getHours()) {
+                    timeNow.setDate(timeNow.getDate() + 1);
+                }
+                
+                timeNow.setHours(hours);
+                timeNow.setMinutes(minutes);
+                timeNow.setSeconds(seconds);
+                timeToAttack.setHours(timeToAttack.getHours() + 1);
+
+                const timeout = Math.abs(timeNow - timeToAttack);
+                console.log("Waiting for:", millisToMinutesAndSeconds(timeout));
+                await new Promise(r => setTimeout(r, timeout));;
             }
             
-            timeNow.setHours(hours);
-            timeNow.setMinutes(minutes);
-            timeNow.setSeconds(seconds);
-            timeToAttack.setHours(timeToAttack.getHours() + 1);
+            // Search for user
+            await page.goto('https://int4.knightfight.moonid.net/raubzug/gegner/?searchuserid=517000943');
+            await page.waitForSelector("#page > form > div > table:nth-child(6) > tbody > tr:nth-child(8) > td > input[type=image]");
+            console.log("Buscou...");
 
-            const timeout = Math.abs(timeNow - timeToAttack);
-            console.log("Waiting for:", millisToMinutesAndSeconds(timeout));
-            await new Promise(r => setTimeout(r, timeout));;
+            // Attack user
+            await page.click("#page > form > div > table:nth-child(6) > tbody > tr:nth-child(8) > td > input[type=image]");
+            console.log("Atacou...");
+
+            // Skip button
+            await page.waitForSelector("#button-skip-prem");
+            await page.click("#button-skip-prem");
+            
+            // Get attack time
+            await page.waitForSelector("#page > div > div:nth-child(4) > div > div > div.kf-bi-thin.padding-md.margin-top-lg.cc.f-os > span > b");
+            const valueTime = await page.$eval("#page > div > div:nth-child(4) > div > div > div.kf-bi-thin.padding-md.margin-top-lg.cc.f-os > span > b", el => el.innerHTML);
+            timeToAttack = kfAttackTimeToFullDate(valueTime);
+        } catch (error) {
+            console.error("Error:", error);
         }
-        
-        // Search for user
-        await page.goto('https://int4.knightfight.moonid.net/raubzug/gegner/?searchuserid=517000943');
-        await page.waitForSelector("#page > form > div > table:nth-child(6) > tbody > tr:nth-child(8) > td > input[type=image]");
-        console.log("Buscou...");
-
-        // Attack user
-        await page.click("#page > form > div > table:nth-child(6) > tbody > tr:nth-child(8) > td > input[type=image]");
-        console.log("Atacou...");
-
-        // Skip button
-        await page.waitForSelector("#button-skip-prem");
-        await page.click("#button-skip-prem");
-        
-        // Get attack time
-        await page.waitForSelector("#page > div > div:nth-child(4) > div > div > div.kf-bi-thin.padding-md.margin-top-lg.cc.f-os > span > b");
-        const valueTime = await page.$eval("#page > div > div:nth-child(4) > div > div > div.kf-bi-thin.padding-md.margin-top-lg.cc.f-os > span > b", el => el.innerHTML);
-        timeToAttack = kfAttackTimeToFullDate(valueTime);
 
         // Remove account
         await page.goto("https://int4.knightfight.moonid.net/profil/");
